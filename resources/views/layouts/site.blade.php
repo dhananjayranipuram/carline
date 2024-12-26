23e17ac82418c716170f8cea4f9d9f968c262020
dhananjaydevmonkey <dhananjaya.k@devmonkey.in>
2024-12-26 15:02:50 +0530

    fix

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -165,11 +165,10 @@
         align-items: center;
         justify-content: center;
         padding: 8px 10px;
-        border-radius: 6px;
-        background-color: #000080;
-        color: #ffffff;
+        color: #000080;
         font-size: 22px;
         text-decoration: none;
+        margin-right: -20px;
     }
 }
 @media (min-width: 769px) {
